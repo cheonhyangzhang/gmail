664e74145d7d1844216698156186e469f3d7e359
Frank Zhang <txzhang@motorola.com>
2015-03-27 17:02:08 -0500

    Bootstrap the basic UI of Gmail

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = document.querySelector('#app');
+app.heading = 'Gmail Inbox';
+
+
+toggleDrawer = function(){
+	drawer = document.querySelector('#drawerPanel');
+	drawer.togglePanel();
+}
+
+
+app.data = [
+{'name':'frank'},
+{'name':'ava'},
+{'name':'frank'},
+{'name':'ava'},
+{'name':'frank'},
+{'name':'ava'},
+{'name':'frank'},
+{'name':'ava'},
+{'name':'frank'},
+{'name':'ava'},
+{'name':'frank'},
+{'name':'ava'},
+{'name':'frank'},
+{'name':'ava'},
+]
+
+
